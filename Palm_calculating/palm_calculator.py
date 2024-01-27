3ed3f6bf1649d4c5fe38759ad48d4aa2d35a7659
Bolci <Bolci@github.com>
2024-01-27 17:18:27 +0100

    add comments

--- Palm_calculating/palm_calculator.py
+++ Palm_calculating/palm_calculator.py
@@ -9,11 +9,17 @@ import torch
 
 
 class PalmCalculator:
-    def __init__(self, ):
+    def __init__(self):
         self.model = None
 
     @staticmethod
-    def detect_peaks(image):
+    def detect_peaks(image: np.array) -> np.array:
+        """
+        Detects peaks in a given image using a maximum filter and binary erosion for peak detection.
+
+        :param image: Input image where peaks are to be detected.
+        :return: A boolean array with True at peaks locations.
+        """
         neighborhood = generate_binary_structure(2, 2)
         local_max = maximum_filter(image, footprint=neighborhood) == image
         background = (image == 0)
@@ -22,31 +28,65 @@ class PalmCalculator:
 
         return detected_peaks
 
-    def set_model(self, config_path, checkpoint_path, device='cpu'):
+    def set_model(self, config_path: str, checkpoint_path: str, device: str = 'cpu') -> None:
+        """
+        Loads a model for palm tree segmentation using provided configuration and checkpoint paths.
+
+        :param config_path: Path to the model's configuration file.
+        :param checkpoint_path: Path to the model's checkpoint file.
+        :param device: Device to run the model on ('cpu' or 'cuda').
+        """
         config_loaded = Config.fromfile(config_path)
         self.model = init_model(config_loaded, checkpoint_path, device=device)
 
     @torch.no_grad()
-    def inference(self, img_loaded, class_id=1) -> tuple:
+    def inference(self, img_loaded: np.array, class_id: int = 1) -> tuple:
+        """
+        Performs inference on an image using the loaded model.
+
+        :param img_loaded: The loaded image for inference.
+        :param class_id: The class ID to be considered for segmentation. Default is 1.
+        :return: A tuple containing the class map and logits map.
+        """
+
         result = inference_model(self.model, img_loaded)
         class_map = result.pred_sem_seg.data[0].detach().cpu().numpy()
         logits_map = result.seg_logits.data.detach().cpu().numpy()
 
-        return class_map, logits_map[1]
+        return class_map, logits_map[class_id]
 
     @staticmethod
-    def threshold_logits(logits_map, threshold=0):
+    def threshold_logits(logits_map: np.array, threshold: int = 0) -> np.array:
+        """
+        Applies a threshold to the logits map.
+
+        :param logits_map: The logits map to be thresholded.
+        :param threshold: The threshold value. Default is 0.
+        :return: The thresholded logits map.
+        """
         log_min_max = copy(logits_map)
         log_min_max[logits_map <= threshold] = 0
         return log_min_max
 
-    def get_peaks(self, logits_map):
+    def get_peaks(self, logits_map: np.array) -> np.array:
+        """
+        Applies Gaussian blurring and detects peaks in the logits map.
+
+        :param logits_map: The logits map for peak detection.
+        :return: A boolean array indicating the peaks in the logits map.
+        """
         blured_img = cv2.GaussianBlur(logits_map, (5, 5), 0)
         peaks = self.detect_peaks(blured_img)
 
         return peaks
 
-    def calculate_palms(self, loaded_img, image_path):
+    def calculate_palms(self, loaded_img: np.array) -> int:
+        """
+        Calculates the number of palm clusters in an image.
+
+        :param loaded_img: The loaded image for palm calculation.
+        :return: The number of detected palm clusters.
+        """
         class_map, logits = self.inference(loaded_img)
         log_min_max = self.threshold_logits(logits)
         peaks = self.get_peaks(log_min_max)
@@ -55,26 +95,53 @@ class PalmCalculator:
         return n_of_clusters
 
     @staticmethod
-    def calculate_distance_from_points(img_coord, point):
+    def calculate_distance_from_points(img_coord: np.array, point: np.array) -> np.array:
+        """
+        Calculates Euclidean distance from each point in the image to a specific point.
+
+        :param img_coord: Coordinates in the image.
+        :param point: The specific point to calculate distance to.
+        :return: Array of distances.
+        """
         new_coord = copy(img_coord)
         new_point = copy(point)
         return np.sqrt(np.sum(np.power(new_coord - new_point, 2), axis=1))
 
     @staticmethod
-    def assign_to_cluster(distances):
+    def assign_to_cluster(distances:np.array) -> np.array:
+        """
+        Assign each point to the nearest cluster based on distances.
+
+        :param distances: Distances of points to cluster centers.
+        :return: Array of cluster indices each point is assigned to.
+        """
         dist = copy(distances)
         return np.argmin(dist, axis=0)
 
     @staticmethod
-    def draw_points(new_img, i, j):
+    def draw_points(new_img: np.array, i: np.array, j: np.array) -> np.array:
+        """
+        Draw points on an image at specified coordinates.
+
+        :param new_img: Image to draw points on.
+        :param i: Array of i (row) coordinates.
+        :param j: Array of j (column) coordinates.
+        :return: Image with points drawn.
+        """
         n_of_clusters = len(i)
         for x_cluster in range(n_of_clusters):
             point_cluster = np.asarray([i[x_cluster], j[x_cluster]])
             new_img = cv2.circle(new_img, point_cluster, radius=4, color=(255), thickness=-1)
         return new_img
 
-    def segment_palms(self, loaded_img, image_path):
+    def segment_palms(self, loaded_img: np.array, image_path: str) -> tuple:
+        """
+        Segment palm trees in an image and calculate their coordinates.
 
+        :param loaded_img: Loaded input image.
+        :param image_path: Path to the input image.
+        :return: Tuple with segmented image, logits, point image, number of clusters, and peaks coordinates.
+        """
         class_map, logits = self.inference(loaded_img, image_path)
         log_min_max = self.threshold_logits(logits)
         peaks = self.get_peaks(log_min_max)
